5db76a38a871dd5b349e7c2724ab9f12bdb3e34d
João <joaovitorresende.jr@gmail.com>
2026-08-18 10:28:50 -0300

    feat: adding imaga and creating render logic when added

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,12 @@ import InpuText from "./components/input-text";
 import SearchIcon from "./assets/icons/search.svg?react"
 import InputCheckbox from "./components/input-checkbox";
 import InputSingleFile from "./components/input-single-file";
+import { useForm } from "react-hook-form";
 
 
 
 export default function App() {
+	const form = useForm();
 	return (
 		<div className="grid gap-7 p-6">
 			<div className="flex gap-3">
@@ -64,7 +66,7 @@ export default function App() {
 			</div>
 
 			<div>
-				<InputSingleFile/>
+				<InputSingleFile form={form} {...form.register("file")}/>
 			</div>
 		</div>
 	);
