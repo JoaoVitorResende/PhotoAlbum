a0f8b1c13aaa5bf376f375fd9ed331d608804b02
João <joaovitorresende.jr@gmail.com>
2026-08-25 10:25:22 -0300

    feat: photolist created to organize photos on home page

--- src/Pages/page-home.tsx
+++ src/Pages/page-home.tsx
@@ -1,21 +1,20 @@
 import Container from "../components/container";
-import PhotoWidget from "../contexts/photos/components/photo-widget";
+import PhotoList from "../contexts/photos/components/photo-list";
 
 export default function PageHome(){
     return(
         <Container>
-            <div className="grid grid-cols-4 gap-9">
-                <PhotoWidget photo={{
+           <PhotoList photos={[
+            {
                 id: '23',
                 title: 'teste',
                 imageId: 'portrait-tower.png',
-                albums:[
-                    {id: '1', title: 'Album 1'},
-                    {id: '2', title: 'Album 2'},
-                    {id: '3', title: 'Album 3'}
-                ]
-            }}/>
-            </div>
+                albums: [
+                    { id: '1', title: 'Album 1' },
+                    { id: '2', title: 'Album 2' },
+                    { id: '3', title: 'Album 3' }
+                ]}
+           ]}/>
         </Container>
     )
 }
