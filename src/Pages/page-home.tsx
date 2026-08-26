3869bc2c95419ec104bd4b57006284ae441a105f
João <joaovitorresende.jr@gmail.com>
2026-08-26 10:34:01 -0300

    feat: filtro de albums

--- src/Pages/page-home.tsx
+++ src/Pages/page-home.tsx
@@ -1,9 +1,13 @@
 import Container from "../components/container";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
 import PhotoList from "../contexts/photos/components/photo-list";
 
 export default function PageHome(){
     return(
         <Container>
+            <AlbumsFilter albums={
+                [{ id: '1', title: 'Album 1' },]} 
+            className="mb-9"/>
            <PhotoList photos={[
             {
                 id: '23',
