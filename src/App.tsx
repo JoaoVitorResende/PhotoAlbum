37ed3b0cff916651fc84944a7163323f3091cc71
João <joaovitorresende.jr@gmail.com>
2026-08-28 11:23:07 -0300

    feat: creating data with postman, addding tanstack to fetcher and evn to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import {BrowserRouter, Route, Routes} from "react-router"
+import { BrowserRouter, Route, Routes } from "react-router"
 import PageComponents from "./Pages/page-components"
 import LayoutMain from "./Pages/layout-main"
 import PageHome from "./Pages/page-home"
 import PhotoDetails from "./Pages/page-photo-details"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+
+const queryClient = new QueryClient();
+
 
 export default function App() {
-	return(
-		<BrowserRouter>
-			<Routes>
-				<Route element={<LayoutMain/>}>
-					<Route index element={<PageHome/>}/>
-					<Route path="/photos/:id" element={<PhotoDetails/>}/>
-					<Route path="/components" element={<PageComponents/>}/>
-				</Route>
-			</Routes>
-		</BrowserRouter>
+	return (
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<LayoutMain />}>
+						<Route index element={<PageHome />} />
+						<Route path="/photos/:id" element={<PhotoDetails />} />
+						<Route path="/components" element={<PageComponents />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	)
 }
