6dedd791e8c661a8730c642712b6d16269fa8253
João <joaovitorresende.jr@gmail.com>
2026-08-31 09:52:45 -0300

    feat: adding nuqs adapters

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import LayoutMain from "./Pages/layout-main"
 import PageHome from "./Pages/page-home"
 import PhotoDetails from "./Pages/page-photo-details"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
+import {NuqsAdapter} from "nuqs/adapters/react-router/v7"
 
 const queryClient = new QueryClient();
 
@@ -12,15 +12,17 @@ const queryClient = new QueryClient();
 export default function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<Routes>
-					<Route element={<LayoutMain />}>
-						<Route index element={<PageHome />} />
-						<Route path="/photos/:id" element={<PhotoDetails />} />
-						<Route path="/components" element={<PageComponents />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<NuqsAdapter>
+				<BrowserRouter>
+					<Routes>
+						<Route element={<LayoutMain />}>
+							<Route index element={<PageHome />} />
+							<Route path="/photos/:id" element={<PhotoDetails />} />
+							<Route path="/components" element={<PageComponents />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</NuqsAdapter>
 		</QueryClientProvider>
 	)
 }
