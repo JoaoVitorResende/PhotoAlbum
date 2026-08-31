e04950f43b0565a7f4a9e259091751573b7261ce
João <joaovitorresende.jr@gmail.com>
2026-08-31 14:04:05 -0300

    feat: updating album filter when click on the button filter

--- src/contexts/albums/components/albums-filter.tsx
+++ src/contexts/albums/components/albums-filter.tsx
@@ -1,6 +1,7 @@
 import Button from "../../../components/button";
 import Skeleton from "../../../components/skeleton";
 import Text from "../../../components/text";
+import usePhotos from "../../photos/hooks/use-photos";
 import type { Album } from "../models/album"
 import cx from "classnames";
 
@@ -10,19 +11,25 @@ interface AlbumsFilterProps extends React.ComponentProps<"div"> {
 }
 
 export default function AlbumsFilter({ albums, loading, className, ...props }: AlbumsFilterProps) {
+    const {filters} = usePhotos();
     return (
         <div className={cx("flex items-center gap-3.5 overflow-x-auto", className)} {...props}>
             <Text variant="heading-small">Albuns</Text>
             <div className="flex gap-3">
                 {!loading ? (
                     <>
-                        <Button variant="primary" size="sm" className="cursor-pointer"> todos </Button>
+                        <Button variant={filters.albumId === null ? "primary" : "ghost"}
+                         size="sm"
+                          className="cursor-pointer"
+                            onClick={() => filters.setalbumId(null)}> todos </Button>
                         {albums.map((album) => (
                             <Button
                                 key={album.id}
-                                variant="ghost"
+                                variant={filters.albumId === album.id ? "primary" : "ghost"}
                                 size="sm"
-                                className="cursor-pointer">
+                                className="cursor-pointer"
+                                onClick={() => filters.setalbumId(album.id)}
+                                >
                                 {album.title}
                             </Button>
                         ))}
